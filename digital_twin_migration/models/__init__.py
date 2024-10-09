6f081d96312a90c3e2bfcbe008293cd8eb59717b
Ferdi761 <ferdiansyahachmad29@gmail.com>
2024-10-09 13:41:47 +0700

    feat: risk matrix migration

--- digital_twin_migration/models/__init__.py
+++ digital_twin_migration/models/__init__.py
@@ -4,5 +4,5 @@ from digital_twin_migration.database import db
 import digital_twin_migration.models.authentication
 import digital_twin_migration.models.efficiency_app
 import digital_twin_migration.models.pfi_app
-
+import digital_twin_migration.models.risk_matrix_app
 # ? =======================================
